f63d381b36184589470e79be0bb6f9ea5b9d2bbb
reerant <43791491+reerant@users.noreply.github.com>
2019-12-05 16:56:01 +0200

    added TMDb attribution to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-    <p><a href="https://www.themoviedb.org/documentation/api">Powered by The Movie Database</a></p>
+    <p>This product uses <a href="https://www.themoviedb.org/documentation/api">the TMDb API</a> but is not endorsed or certified by TMDb.</p>
   </div>
   );
 }
